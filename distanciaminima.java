bdb6e1620c1ac1db0a9730b4bebf1a11f9eaaee6
Belen321 <166552086+Belen321@users.noreply.github.com>
2024-04-09 14:11:57 -0600

    hola

--- distanciaminima.java
+++ distanciaminima.java
@@ -1,7 +1,30 @@
 
 public class distanciaminima {
+
+    public Coordenadas(int x, int y)
+            {
+                int x; 
+                int y;
+                this.x = x; 
+                this.y =y; 
+            }
     public static void main(String[] args) {
-        
-    }
+            Scanner leer=new Scanner(System.in); 
+            Lectura leer=new Lectura(); 
+            System.out.println("Ingrese la los objetos de la Clase 1 "); 
+            System.out.println("(x,y)"); 
+
+             
+            Coordenada[] coordenada = new Coordenadas[3]; 
+
+            Coordenada[0] coordenada = new Coordenadas[1,3];
+            Coordenada[1] coordenada = new Coordenadas[2,3];
+            Coordenada[2] coordenada = new Coordenadas[3,3];
+
+             for (Coordenada c : coordenadas) 
+            {
+                 System.out.println(c);
+            }
 }
->>>>>>> a8ea23294a3711489c2eb45ae81116a3e1b186f0
+
+}
