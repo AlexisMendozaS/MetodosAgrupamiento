5d89aab166869e8360792dbffe5a75ee506195ab
AlexisMendozaS <armandomendoza988@gmail.com>
2024-04-12 16:11:59 -0600

    distancia manhattan

--- KNN.java
+++ KNN.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class KNN {
@@ -14,24 +16,26 @@ public class KNN {
         // Loop through each class
         for (int i = 0; i < numClasses; i++) {
             // Prompt the user to enter the size of the class
-            System.out.print("Ingresa el tamaño de la clase " + (i+1) + ": ");
+            System.out.print("Ingresa el tamaño de la clase " + (i + 1) + ": ");
             int classSize = scanner.nextInt();
 
             // Create a 2D array to store the data for this class
-            data[i] = new double[classSize][2];
+            data[i] = new double[classSize][3];
 
             // Loop through each element in the class
             for (int j = 0; j < classSize; j++) {
                 // Prompt the user to enter the coordinates of the element
-                System.out.print("Ingresa la coordenada del elemento " + (j+1) + ": ");
+                System.out.print("Ingresa la coordenada del elemento " + (j + 1) + ": ");
                 double x = scanner.nextDouble();
                 double y = scanner.nextDouble();
 
                 // Store the coordinates in the 2D array
                 data[i][j][0] = x;
                 data[i][j][1] = y;
+                data[i][j][2] = i + 1; // Store the class number in the third index
             }
         }
+
         // Prompt the user to enter the value of k
         System.out.print("Ingresa el valor de k: ");
         int k = scanner.nextInt();
@@ -46,7 +50,7 @@ public class KNN {
         // Loop through each new point
         for (int i = 0; i < numNewPoints; i++) {
             // Prompt the user to enter the coordinates of the new point
-            System.out.print("Ingresa la coordenada del nuevo punto " + (i+1) + ": ");
+            System.out.print("Ingresa la coordenada del nuevo punto " + (i + 1) + ": ");
             double x = scanner.nextDouble();
             double y = scanner.nextDouble();
 
@@ -56,18 +60,18 @@ public class KNN {
         }
 
         // Implement the k-NN algorithm here!
-        
-        // The k-NN algorithm goes here!
-        // For now, we'll just print out the data we collected
-        for (int i = 0; i < numClasses; i++) {
-            System.out.println("Class " + (i+1) + ":");
-            for (int j = 0; j < data[i].length; j++) {
-                System.out.println("(" + data[i][j][0] + ", " + data[i][j][1] + ")");
-            }
-        }
-        // For now, we'll just print out the data we collected
+        // Calculate the Manhattan distance between the new points and all the objects in all the classes
         for (int i = 0; i < numNewPoints; i++) {
-            System.out.println("New point " + (i+1) + ": (" + newPoints[i][0] + ", " + newPoints[i][1] + ")");
+            for (int j = 0; j < numClasses; j++) {
+                for (int l = 0; l < data[j].length; l++) {
+                    // Calculate the Manhattan distance
+                    double d = Math.abs(newPoints[i][0] - data[j][l][0]);
+                    double d2 = Math.abs(newPoints[i][1] - data[j][l][1]);
+                    double distance = d + d2;
+                    // Print the distance
+                    System.out.println("d" + (l+1) + "(On" + (i+1) + ",O" + (l+1) + ") = |" + newPoints[i][0] + "-" + data[j][l][0] + "| + |" + newPoints[i][1] + "-" + data[j][l][1] + "| = " + d + " + " + d2 + " = " + distance);
+                }
+            }
         }
     }
 }
